c7b82aade42b4b76bad808dafebc734dead689d6
humayunadilshahzad <35384625+humayunadilshahzad@users.noreply.github.com>
2018-12-07 20:51:56 +0100

    updated file

--- javascript/javascript1/week1/startup-names.js
+++ javascript/javascript1/week1/startup-names.js
@@ -1,10 +1,12 @@
 let firstWords=["Giga","Unique","Popular","Star","Selective","Dandy","Zeropoint","Analytic","Trendsetter","HiTech"];
 let secondWords=["Solutions","Limited","Corporations","Town","Globe","Valley","Blend","Corporate","Ways","Designs"];
-
+let startupName,starupLength;
 for(let i=0;i<10;i++)
     {
     const randomNumber1 = Math.floor(Math.random() * 10);
     const randomNumber2 = Math.floor(Math.random() * 10);
-    console.log(firstWords[randomNumber1]+" "+secondWords[randomNumber2]);
+    startupName=firstWords[randomNumber1]+" "+secondWords[randomNumber2];
+    starupLength=startupName.length;
+    console.log("Name : "+startupName+"\t\tLength= "+starupLength);
     }
   
