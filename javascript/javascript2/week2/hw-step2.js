e41699c4d4275a5c4b9323665a3394ee760728f4
humayunadilshahzad <35384625+humayunadilshahzad@users.noreply.github.com>
2019-01-19 22:05:57 +0100

    multiple of 3 or 5

--- javascript/javascript2/week2/hw-step2.js
+++ javascript/javascript2/week2/hw-step2.js
@@ -1,6 +1,11 @@
 let myButton=document.getElementsByTagName("button");
 myButton[0].addEventListener("click",() => {
     let enteredNumber= document.getElementsByTagName("input")[0].value;
+    if (enteredNumber=="")
+       { alert("must enter a value first");
+        document.getElementsByTagName("input")[0].focus();}
+
+    else    
     myFunction( enteredNumber, isThreeMultiple, isFiveMultiple);
 });
 
@@ -24,6 +29,8 @@ function myFunction(n, isThree, isFive){
             isThree();
         else if(n%5 ==0)
             isFive(); 
+        else
+            alert("not a multiple of 3 of 5");
 }
 const isThreeMultiple = function(){
     document.body.style.backgroundColor="blue";
@@ -33,3 +40,28 @@ const isFiveMultiple = () => {
     document.body.style.backgroundColor="red";
 }
 
+
+
+
+/***************************************/ 
+
+/* let functionArray=[ add= (a,b)=>{console.log(a+b); },sub= (a,b)=>{console.log(a-b)},print=function(){console.log("bismillah")}];
+for(let i=0 ; i< functionArray.length ; i++)
+    if(i<2)
+        functionArray[i](5,5);
+    else
+        functionArray[i];
+
+console.log(typeof(functionArray[0]));
+console.log(typeof(functionArray[1]));
+console.log(typeof(functionArray[2]));
+ */        
+/************************************************ */
+ /*
+function myFun(){
+    console.log("normal function called");
+}
+
+const myConstFun(){
+
+} */
