ff255c1132bf5e351f2a223bf062e15a6b61501b
humayunadilshahzad <35384625+humayunadilshahzad@users.noreply.github.com>
2020-02-07 20:57:59 +0100

    dead code removed

--- react/week1/src/App.js
+++ react/week1/src/App.js
@@ -8,38 +8,9 @@ import MountingTodos from './components/simple-todos'
 function App() {
   return (
     <div className="App">
-     {/* <NameObj/>
-     <ArryState/> */}
      <Todos/>
-     {/* <MountingTodos/> */}
-
     </div>
   );
 }
 
 export default App;
-
-/* <Todos name='adil' name1='shahzada'> <button>click</button> </Todos>
-<Todos name='basil' name1='chona' />
-<Todos name='rafeh' name1='paru' />
- */
-
-
-//DEFAULT APP.JS PRE CODE RETURN JSX
-
-// <div className="App">
-// <header className="App-header">
-//   <img src={logo} className="App-logo" alt="logo" />
-//   <p>
-//     Edit <code>src/App.js</code> and save to reload.
-//   </p>
-//   <a
-//     className="App-link"
-//     href="https://reactjs.org"
-//     target="_blank"
-//     rel="noopener noreferrer"
-//   >
-//     Learn React
-//   </a>
-// </header>
-// </div>
